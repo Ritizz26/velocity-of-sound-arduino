dfba56459a0ddf4bec0a5aa7207c413dd53954c3
Ritiz Shrestha <ritizshrestha321@gmail.com>
2026-08-13 20:02:47 +0545

    Add files via upload

---
+++ VelocityOfSoundCode.ino
@@ -0,0 +1,40 @@
+#include<LiquidCrystal_I2C.h>
+#include<DHT.h>
+DHT dht(4,DHT11);
+LiquidCrystal_I2C lcd(0x27,16,2);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  dht.begin();
+  pinMode(7,OUTPUT);
+  pinMode(2,INPUT); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //DHT11
+  float t= dht.readTemperature();
+
+  //HCSRO4
+  digitalWrite(7,LOW);
+  delayMicroseconds(2);
+  digitalWrite(7,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(7,LOW);
+  
+  long time= pulseIn(2,HIGH,30000);
+  float velocity = (2*29.3)/time;
+  float v= velocity*10000.0;
+
+  //LCD
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.print("'C");
+  lcd.setCursor(0,1);
+  lcd.print("Speed:");
+  lcd.print(v);
+  lcd.print("m/s");
+  delay(5000);
+}
